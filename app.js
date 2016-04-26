3d36408ff64dfe20946ad41ef86e70840c032e30
Joao Paulo Jung Ramia <joao.ramia@gmail.com>
2016-04-26 18:02:48 -0400

    fourth commit

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var express = require( 'express' );
+var swig = require('swig');
+var routes = require('./routes');
+
 var app = express(); // creates an instance of an express application
+
+swig.setDefaults({ cache: false });
+
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', './views');
 
 app.use(function(req, res, next){
 	console.log(req.method, req.url);
@@ -12,14 +22,25 @@ app.use("/special", function(req, res, next){
 	next();
 })
 
-app.get("/", function(req, res){
-	res.send("Welcome");
-	// console.log(res.status());
-})
+app.use('/', routes);
 
-app.get("/news", function(req, res){
-	res.send("Good news");
-})
+
+app.use(express.static(__dirname + '/public'));
+// app.get("/", function(req, res){
+// 	res.send("Welcome");
+// 	// console.log(res.status());
+// })
+
+// app.get("/news", function(req, res){
+// 	res.render('index', {
+// 	title: "An Example",
+// 	people: [{name: "Gandalf"},{name: "Frodo"}, {name: "Hermione"}]
+// 	}, function (err, html){
+// 	if(err) throw err;
+// 	res.send(html);
+// })
+
+// })
 
 app.listen(3000, function(){
 	console.log("server listening");
